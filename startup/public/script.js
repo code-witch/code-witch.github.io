9a9aeec58e8677c8342eb84e94bfdcde4c370f1e
code-witch <code-witch@users.noreply.github.com>
2022-11-02 04:30:32 -0600

    blur, date, muah

--- startup/public/script.js
+++ startup/public/script.js
@@ -77,10 +77,56 @@ const setupImage = () => {
 
 const time = () => {
     let now = new Date();
+    let day = now.getDay();
+    let month = now.getMonth();
+    let date = now.getDate();
+
+    switch (date) {
+        case 1: case 21: case 31:
+            date = `${date}st`;
+            break;
+        case 2: case 22: case 32:
+            date = `${date}nd`;
+            break;
+        case 3: case 23:
+            date = `${date}rd`
+            break;
+        default:
+            date = `${date}th`;
+            break;
+    }
+
+
+    switch (day) {
+        case 0:  day = 'Sun'; break;
+        case 1:  day = 'Mon'; break;
+        case 2:  day = 'Tue'; break;
+        case 3:  day = 'Wed'; break;
+        case 4:  day = 'Thu'; break;
+        case 5:  day = 'Fri'; break;
+        case 6:  day = 'Sat'; break;
+        default: day = 'NaN'; break;
+    };
+    switch (month) {
+        case 0:  month = 'Jan'; break;
+        case 1:  month = 'Feb'; break;
+        case 2:  month = 'Mar'; break;
+        case 3:  month = 'Apr'; break;
+        case 4:  month = 'May'; break;
+        case 5:  month = 'Jun'; break;
+        case 6:  month = 'Jul'; break;
+        case 7:  month = 'Aug'; break;
+        case 8:  month = 'Sep'; break;
+        case 9:  month = 'Oct'; break;
+        case 10: month = 'Nov'; break;
+        case 11: month = 'Dec'; break;
+        default: month = 'NaN'; break;
+    }
+
     let h = String(now.getHours()).padStart(2,'0');
     let m = String(now.getMinutes()).padStart(2,'0');
     let s = String(now.getSeconds()).padStart(2,'0');
-    clock.innerHTML = `${h >= 13 ? h - 12 : h}:${m}:${s} ${h >= 12 ? 'PM': 'AM'}`
+    clock.innerHTML = `${day}, ${month} ${date} | ${h >= 13 ? h - 12 : h}:${m}:${s} ${h >= 12 ? 'PM': 'AM'}`
     delete now;
 }
 
